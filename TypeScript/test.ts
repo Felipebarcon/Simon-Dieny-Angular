cdb5f7cf367bbeaf4b221e7e20f13e1978bfc9f0
Felipe Barcon <felipe.barcon@gmail.com>
2022-10-30 17:10:52 +0100

    add hero class and hero const

--- TypeScript/test.ts
+++ TypeScript/test.ts
@@ -7,14 +7,22 @@ document.body.innerHTML = sayHello(Jean);
 
 ////////////////////////////////
 
-const lifePoint: number = 100;
-lifePoint = "100";
-const fullName: string = "Green Lantern";
+// Un constructeur pour notre classe Hero
 
-class Hero {
-  constructor(lifePoint: number, fullName: string) {}
+// On spécifie le type de retour après les ':', ici Hero.
+function createHero(lifePoint: number, name: string, planet?: string): Hero {
+  const hero = new Hero();
+  hero.lifePoint = lifePoint;
+  hero.name = name;
+  if (planet) hero.planet = planet;
+  return hero;
 }
+
+const lifePoint: number = 100;
+const fullName: string = "Green Lantern";
 
 const greenLantern: Hero = new Hero(lifePoint, fullName);
 const superMan: Hero = new Hero(lifePoint, "Superman");
 const heros: Array<Hero> = [greenLantern, superMan];
+
+class Test {}
