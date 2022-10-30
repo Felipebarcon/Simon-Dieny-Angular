f47e866cdc4c6bc6505d1c95f97de8d2b590ce61
Felipe Barcon <felipe.barcon@gmail.com>
2022-10-30 10:56:43 +0100

    add hero class and hero const

--- TypeScript/test.js
+++ TypeScript/test.js
@@ -3,3 +3,15 @@ function sayHello(person) {
 }
 var Jean = "Jean";
 document.body.innerHTML = sayHello(Jean);
+////////////////////////////////
+var lifePoint = 100;
+lifePoint = "100";
+var fullName = "Green Lantern";
+var Hero = /** @class */ (function () {
+    function Hero(lifePoint, fullName) {
+    }
+    return Hero;
+}());
+var greenLantern = new Hero(lifePoint, fullName);
+var superMan = new Hero(lifePoint, "Superman");
+var heros = [greenLantern, superMan];
--- TypeScript/test.ts
+++ TypeScript/test.ts
@@ -4,3 +4,17 @@ function sayHello(person) {
 const Jean = "Jean";
 
 document.body.innerHTML = sayHello(Jean);
+
+////////////////////////////////
+
+const lifePoint: number = 100;
+lifePoint = "100";
+const fullName: string = "Green Lantern";
+
+class Hero {
+  constructor(lifePoint: number, fullName: string) {}
+}
+
+const greenLantern: Hero = new Hero(lifePoint, fullName);
+const superMan: Hero = new Hero(lifePoint, "Superman");
+const heros: Array<Hero> = [greenLantern, superMan];
