a98d85a162992ffcb949850d6464cc51726b93d3
Felipe Barcon <felipe.barcon@gmail.com>
2022-10-30 10:43:18 +0100

    add ts test file and compilate in js file

---
+++ TypeScript/test.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return "Hello ".concat(person);
+}
+var Jean = "Jean";
+document.body.innerHTML = sayHello(Jean);
---
+++ TypeScript/test.ts
@@ -0,0 +1,6 @@
+function sayHello(person) {
+  return `Hello ${person}`;
+}
+const Jean = "Jean";
+
+document.body.innerHTML = sayHello(Jean);
